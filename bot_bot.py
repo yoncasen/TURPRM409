528e015092091781c115f6d63081d41c5a574f16
Yonca <73234933+yoncasen@users.noreply.github.com>
2026-03-19 21:18:49 +0300

    Add files via upload

---
+++ bot_bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from bot_token import TOKEN
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run(TOKEN)
